ce6150b56aacd7cdc42f9ad0dd38e89de3b4cf88
Joca Koyi <jocakoyi@MacBook-Pro.home>
2021-05-08 08:53:20 +0200

    d

--- src/cmd/api-ch-react-native/bootstrap.js
+++ src/cmd/api-ch-react-native/bootstrap.js
@@ -1,24 +1,24 @@
 // const config = require('config/cb');
 const http = require('http');
-const { cryptr } = require('encryption');
-const cookieParser = require('cookie-parser');
-const httpHeadersPlugin = require('apollo-server-plugin-http-headers');
+// const { cryptr } = require('encryption');
+// const cookieParser = require('cookie-parser');
+// const httpHeadersPlugin = require('apollo-server-plugin-http-headers');
 const app = require('server')();
 const { graphqlServer } = require('server-graphql');
 const {
-  GraphQLImplResolvers,
-  GraphQLImplTypeDefs,
+  GraphQLResolvers,
+  GraphQLTypeDefs,
   // SessionDirectives,
   // SessionTypeDefs,
-} = require('graphql-implementation');
+} = require('graphql-ch-react-native');
 
 module.exports = () => {
   const apolloSrv = graphqlServer({
-    typeDefs: [SessionTypeDefs, GraphQLImplTypeDefs],
-    schemaDirectives: {
-      ...SessionDirectives,
-    },
-    subscriptions: {
+    typeDefs: GraphQLTypeDefs,
+    // schemaDirectives: {
+    //   ...SessionDirectives,
+    // },
+    // subscriptions: {
       // onConnect: (cnxnPramas, webSocket, cnxnContect) => {
       //   const { cookie } = webSocket.upgradeReq.headers;
       //   if (!cookie) {
@@ -35,72 +35,70 @@ module.exports = () => {
       //     sub, session_id, implementation_id, scheduled_call_id,
       //   };
       // },
-    },
-    resolvers: {
-      ...GraphQLImplResolvers,
-    },
-    plugins: [httpHeadersPlugin],
-    context: async ({ connection, req }) => {
-      if (connection) {
-        return connection.context;
-      }
-      let sessionCookie = false;
-      let sessionCookieRaw = false;
-      // prototype is stripped on req objects
-      if (Object.prototype.hasOwnProperty.call(req.cookies, 'qooqie_cmb_ses')) {
-        sessionCookie = JSON.parse(cryptr(.............................................
-          ...........................
-          ..................
-          .............................).decrypt(req.cookies.qooqie_cmb_ses));
-        sessionCookieRaw = req.cookies.qooqie_cmb_ses;
-      }
+    // },
+    resolvers: GraphQLResolvers,
+    // plugins: [httpHeadersPlugin],
+    // context: async ({ connection, req }) => {
+    //   if (connection) {
+    //     return connection.context;
+    //   }
+    //   let sessionCookie = false;
+    //   let sessionCookieRaw = false;
+    //   // prototype is stripped on req objects
+    //   if (Object.prototype.hasOwnProperty.call(req.cookies, 'qooqie_cmb_ses')) {
+    //     sessionCookie = JSON.parse(cryptr(.............................................
+    //       ...........................
+    //       ..................
+    //       .............................).decrypt(req.cookies.qooqie_cmb_ses));
+    //     sessionCookieRaw = req.cookies.qooqie_cmb_ses;
+    //   }
 
-      if (!Object.prototype.hasOwnProperty.call(req.headers, 'origin')) {
-        throw new Error('requires a origin');
-      }
+    //   if (!Object.prototype.hasOwnProperty.call(req.headers, 'origin')) {
+    //     throw new Error('requires a origin');
+    //   }
 
-      const { origin } = req.headers;
+    //   const { origin } = req.headers;
 
-      return {
-        origin,
-        sessionCookie,
-        sessionCookieRaw,
-        setCookies: [],
-        setHeaders: [],
-      };
-    },
+    //   return {
+    //     origin,
+    //     sessionCookie,
+    //     sessionCookieRaw,
+    //     setCookies: [],
+    //     setHeaders: [],
+    //   };
+    // },
     playground: true,
   });
 
-  const corsOptions = {
-    credentials: true,
-    async origin(origin, callback) {
-      let domains = []; let
-        error = null;
-      try {
-        const result = await Implementation.distinct(
-          'domain_settings.domain',
-          { 'domain_settings.verified': true },
-        );
-        domains = result.reduce((acc, d) => {
-          acc.push(`${d.indexOf('http') === -1 ? 'https://' : ''}${d}`);
-          return acc;
-        }, []);
-      } catch (err) {
-        error = err;
-      }
-      return callback(error, domains);
-    },
-  };
+  // const corsOptions = {
+  //   credentials: true,
+  //   async origin(origin, callback) {
+  //     let domains = []; let
+  //       error = null;
+  //     try {
+  //       const result = await Implementation.distinct(
+  //         'domain_settings.domain',
+  //         { 'domain_settings.verified': true },
+  //       );
+  //       domains = result.reduce((acc, d) => {
+  //         acc.push(`${d.indexOf('http') === -1 ? 'https://' : ''}${d}`);
+  //         return acc;
+  //       }, []);
+  //     } catch (err) {
+  //       error = err;
+  //     }
+  //     return callback(error, domains);
+  //   },
+  // };
 
-  app.use(cookieParser());
+  // app.use(cookieParser());
   apolloSrv.applyMiddleware({
     app,
-    cors: corsOptions,
+    // cors: corsOptions,
   });
 
   const server = http.createServer(app);
-  apolloSrv.installSubscriptionHandlers(server);
+  // apolloSrv.installSubscriptionHandlers(server);
 
   return server;
 };
